45eb1b066cca02975a0983919b2e0e080144d129
zhuchanglizhu <55585104+zhuchanglizhu@users.noreply.github.com>
2020-03-20 22:55:11 +0800

    Create Administrator_Select_Parking.java

---
+++ Parking_Andriod/Administrator_Select_Parking.java
@@ -0,0 +1,17 @@
+package com.example.myparkingcar.administrator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myparkingcar.R;
+
+public class Look_parking extends AppCompatActivity {
+//根据电话号码查询管理员的停车场信息
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.parking_search_place);
+        setContentView(R.layout.failed);
+    }
+}
