ac54855e66acf5dddfc8ba7d16e4988a96cc5c83
zhuchanglizhu <55585104+zhuchanglizhu@users.noreply.github.com>
2020-03-20 23:23:52 +0800

    Create Driver_Operator.java

---
+++ Parking_Andriod/Driver_Operator.java
@@ -0,0 +1,13 @@
+package com.example.myparkingcar.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Usercar_operate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
